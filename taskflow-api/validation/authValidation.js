b5d09aa7cfd97b9507b351171abd7229788e15ab
sandeep <sandeepjaya028@gmail.com>
2026-07-21 08:36:32 +0530

    Add validation schemas for registration and login

--- taskflow-api/validation/authValidation.js
+++ taskflow-api/validation/authValidation.js
@@ -1,1 +1,33 @@
-// TODO
+const Joi = require("joi");
+
+// Register Validation
+const registerSchema = Joi.object({
+  name: Joi.string()
+    .min(3)
+    .max(50)
+    .required(),
+
+  email: Joi.string()
+    .email()
+    .required(),
+
+  password: Joi.string()
+    .min(6)
+    .max(30)
+    .required()
+});
+
+// Login Validation
+const loginSchema = Joi.object({
+  email: Joi.string()
+    .email()
+    .required(),
+
+  password: Joi.string()
+    .required()
+});
+
+module.exports = {
+  registerSchema,
+  loginSchema
+};
