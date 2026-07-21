b170b7850ea124afa1b4417c0cf07e1d3db6c8d1
sandeep <sandeepjaya028@gmail.com>
2026-07-21 08:34:03 +0530

    Implement authentication routes for register and login

--- taskflow-api/routes/authRoutes.js
+++ taskflow-api/routes/authRoutes.js
@@ -1,1 +1,12 @@
-// TODO
+const express = require("express");
+const router = express.Router();
+
+const authController = require("../controllers/authController");
+
+// Register User
+router.post("/register", authController.register);
+
+// Login User
+router.post("/login", authController.login);
+
+module.exports = router;
