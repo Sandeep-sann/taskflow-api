367648af3e2e35ec199829748964fa21b59fc9b4
sandeep <sandeepjaya028@gmail.com>
2026-07-21 08:35:53 +0530

    Add task routes with authentication middleware
    Implemented task routes with CRUD operations and authentication.

--- taskflow-api/routes/taskRoutes.js
+++ taskflow-api/routes/taskRoutes.js
@@ -1,1 +1,22 @@
-// TODO
+const express = require("express");
+const router = express.Router();
+
+const taskController = require("../controllers/taskController");
+const authMiddleware = require("../middleware/authMiddleware");
+
+// Protect all task routes
+router.use(authMiddleware);
+
+// Get all tasks
+router.get("/", taskController.getTasks);
+
+// Create a new task
+router.post("/", taskController.createTask);
+
+// Update a task
+router.put("/:id", taskController.updateTask);
+
+// Delete a task
+router.delete("/:id", taskController.deleteTask);
+
+module.exports = router;
